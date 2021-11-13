11fffb99bc70e419cf928bb318e26a5b85b590ea
rokuosan <85651386+rokuosan@users.noreply.github.com>
2021-11-13 23:17:51 +0900

    場所移動

---
+++ src/main/java/com/deviseworks/enumerate/SettingCommandsEnum.java
@@ -0,0 +1,20 @@
+package com.deviseworks.enumerate;
+
+public enum SettingCommandsEnum {
+    setMaxMemory("最大使用可能メモリ量"),
+    setMinMemory("最小利用可能メモリ量"),
+    getMaxMemory("現在の最大メモリ量を取得する"),
+    getMinMemory("現在の最小メモリ量を取得する"),
+    exit("設定を終了する");
+
+
+    private final String description;
+
+    SettingCommandsEnum(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+}

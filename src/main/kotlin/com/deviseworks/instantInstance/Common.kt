a96f22a8a1581cb6b52e05e41aadb31bae0aaf59
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-09 18:04:30 +0900

    ビルド取得の機能を実装

--- src/main/kotlin/com/deviseworks/instantInstance/Common.kt
+++ src/main/kotlin/com/deviseworks/instantInstance/Common.kt
@@ -9,8 +9,16 @@ object Common{
     // Prefix
     const val SHELL = "> "
     const val SHELL_INSTALL = "INSTALL $SHELL"
+    const val SHELL_INSTALL_PAPER = "${SHELL_INSTALL}PAPER > "
 
     // Error Message
     const val COMMAND_NOT_FOUND = "\t不明なコマンド\n\thelpでコマンドリストを表示します。"
+    const val INVALID_VALUE = "\t不明な値です"
+
+    // API URL
+    const val PAPER_VERSIONS = "https://api.papermc.io/v2/projects/paper"
+
+    // Directories
+
 
 }
--- src/main/kotlin/com/deviseworks/instantInstance/util/Fetcher.kt
+++ src/main/kotlin/com/deviseworks/instantInstance/util/Fetcher.kt
@@ -1,13 +1,17 @@
 package com.deviseworks.instantInstance.util
 
 import com.deviseworks.instantInstance.Common
+import com.deviseworks.instantInstance.entity.PaperBuilds
 import com.deviseworks.instantInstance.entity.PaperVersions
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.net.HttpURLConnection
 import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
-class Fetcher {
+object Fetcher {
     fun getPaperVersions(): PaperVersions?{
         val connect = URL(Common.PAPER_VERSIONS).openConnection() as HttpURLConnection
         connect.connectTimeout = 20_000
@@ -27,6 +31,42 @@ class Fetcher {
             e.printStackTrace()
             println("Connection Error: Failed to fetch")
             null
+        }
+    }
+
+    fun getPaperBuilds(version: String): PaperBuilds?{
+        val c = URL("${Common.PAPER_VERSIONS}/versions/$version").openConnection() as HttpURLConnection
+        c.connect()
+
+        return try{
+            val sb = StringBuilder()
+            c.inputStream.bufferedReader().use { br ->
+                br.lines().forEach {
+                    sb.append(it)
+                }
+            }
+
+            Json.decodeFromString<PaperBuilds>(sb.toString())
+        }catch(e: Exception){
+            e.printStackTrace()
+            println("Connection Error: Failed to fetch build")
+            null
+        }
+    }
+
+    /**
+     * ファイルをダウンロードする関数
+     *
+     * @param url ダウンロードURL
+     * @param dest ダウンロード先
+     */
+    fun downloadFile(url: String, dest: String){
+        val filename = URL(url).file
+
+        try{
+            Files.copy(URL(url).openStream(), Paths.get("${dest}/$filename"), StandardCopyOption.REPLACE_EXISTING)
+        }catch(e: Exception){
+            e.printStackTrace()
         }
     }
 }

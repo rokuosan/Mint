b94222f5028e65af6e4a1721a86e409a63fad796
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-09 23:38:17 +0900

    起動スクリプト生成を実装

--- src/main/kotlin/com/deviseworks/instantInstance/util/ScriptManager.kt
+++ src/main/kotlin/com/deviseworks/instantInstance/util/ScriptManager.kt
@@ -1,32 +1,42 @@
 package com.deviseworks.instantInstance.util
 
+import java.io.BufferedWriter
+import java.io.FileWriter
+
 object ScriptManager {
-    fun generatePaper(version: String, build: String){
+    fun generatePaper(version: String, build: String, path: String){
+        // Checking OS
         val os = System.getProperty("os.name")
         val isWindows = os.startsWith("windows", true)
+
+        // Create script for booting paper
         val script = if(isWindows){
             buildString {
                 append("@echo off")
+                appendLine()
                 append("java -Xmx4G -Xms4G -server -jar paper-$version-$build.jar nogui")
+                appendLine()
                 append("pause")
             }
         }else{
             buildString {
                 append("#!/bin/sh")
+                appendLine()
                 append("java -Xmx4G -Xms4G -server -jar paper-$version-$build.jar nogui")
             }
         }
 
+        // Script's extension
         val filename = if(isWindows){
-            "start-demo.bat"
+            "$path/start-demo.bat"
         }else{
-            "start-demo.sh"
+            "$path/start-demo.sh"
         }
 
-
-
-
-
-
+        // Flush
+        BufferedWriter(FileWriter(filename)).use { bw ->
+            bw.write(script)
+            bw.flush()
+        }
     }
 }

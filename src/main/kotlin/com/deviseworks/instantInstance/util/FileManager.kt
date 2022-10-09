c70af5acd6c373143a99b9c9217ed69c4d73f6aa
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-09 21:04:47 +0900

    WIP

--- src/main/kotlin/com/deviseworks/instantInstance/util/FileManager.kt
+++ src/main/kotlin/com/deviseworks/instantInstance/util/FileManager.kt
@@ -1,10 +1,9 @@
 package com.deviseworks.instantInstance.util
 
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 
-class FileManager {
+object FileManager {
     /**
      * カレントディレクトリから引数に対応するソフトウェアに対して
      * 作成するインスタンス番号を見つけます。
@@ -14,16 +13,12 @@ class FileManager {
     fun seekNumber(software: String, version: String): Int{
         var tag = 100
         while(true){
-            val path = Paths.get("./${software}/${version}/${tag}/")
+            val path = Paths.get("./${software}/${version}/${tag}/").toAbsolutePath().normalize()
             if(Files.exists(path)){
                 tag++
             }else{
                 return tag
             }
         }
-    }
-
-    fun createInstance(){
-
     }
 }

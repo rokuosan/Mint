54fc12afe338033446ce0f496e637efe2dbe5e4e
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-09 18:03:32 +0900

    パッケージの追加とタスクの追加・編集

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    id("org.jetbrains.dokka") version "1.7.10"
+    id ("com.github.johnrengelman.shadow") version "7.1.2"
     application
+    java
 }
+
+group = "com.deviseworks"
+java.sourceCompatibility = JavaVersion.VERSION_16
+java.targetCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -12,6 +21,9 @@ dependencies {
     // Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
 
+    // Dokka
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.10")
+
     // Test
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
@@ -20,4 +32,19 @@ dependencies {
 application {
     // Define the main class for the application.
     mainClass.set("com.deviseworks.instantInstance.AppKt")
+}
+
+tasks.withType<KotlinCompile>{
+    kotlinOptions{
+        jvmTarget = "11"
+    }
+}
+
+tasks.dokkaHtml.configure{
+    outputDirectory.set(buildDir.resolve("docs"))
+    moduleName.set("Instant Instance Tools")
+}
+
+tasks.getByName<JavaExec>("run"){
+    standardInput = System.`in`
 }

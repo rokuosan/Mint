ff5156efdd6eee9e8898c024fb9502ba68d31d56
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-12-13 00:54:59 +0900

    feat: WIP | Add "new" command

--- src/main/kotlin/io/github/rokuosan/mint/commands/New.kt
+++ src/main/kotlin/io/github/rokuosan/mint/commands/New.kt
@@ -6,13 +6,19 @@ import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.mordant.table.table
 import io.github.rokuosan.mint.fetcher.PaperFetcher
+import io.github.rokuosan.mint.fetcher.PaperFetcherOptions
+import io.github.rokuosan.mint.utils.RandomStringProvider
 
 class New: CliktCommand() {
-    private val engine by option("-e", "--engine", help = "Choose server engine")
+    private val engine by option("-e", "--engine", help = "Server engines(e.g. Paper, Vanilla)")
         .choice("vanilla", "paper")
         .required()
-    private val version by option("-v", "--version", help = "Choose minecraft version")
+    private val version by option("-v", "--version", help = "Minecraft versions (e.g. \"latest\", \"1.20.2\")")
         .required()
+    private val build by option("-b", "--build", help = "Build number (e.g. \"latest\", \"1\")")
+
+    private val destination by option("-d", "--destination", help = "Output directory")
+    private val filename by option("-f", "--filename", help = "Output filename")
 
     override fun run() {
         when (engine) {
@@ -20,12 +26,29 @@ class New: CliktCommand() {
                 echo(table {
                     body { row("Paper", version) }
                 })
-                echo("Checking builds for version $version")
-                val builds = PaperFetcher().getBuilds(version)?:run{
-                    echo("Invalid version")
-                    return
+                val bld = if (build != null) {
+                    build!!
+                }else {
+                    echo("Checking builds for version $version")
+                    val builds = PaperFetcher().getBuilds(version)?:run{
+                        echo("Invalid version")
+                        return
+                    }
+                    echo("Found ${builds.size} builds.")
+                    builds.last().toString()
                 }
-                echo("Found ${builds.size} builds.")
+
+                val dest = if (destination != null) {
+                    destination!!
+                }else {
+                    echo("Using random string as destination directory name.")
+                    val randomString = RandomStringProvider.getShortString()
+                    echo("Random string: $randomString")
+                    randomString
+                }
+
+                echo("Downloading Paper $version build $bld to $dest")
+                PaperFetcher().download(PaperFetcherOptions(version, bld, dest, filename))
             }
             "vanilla", "v" -> {
                 echo("Not implemented.")

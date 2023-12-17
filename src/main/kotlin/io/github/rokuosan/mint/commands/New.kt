69a48dfc5f289069d9cbd678cf1a3b3ab8181b0c
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-12-17 14:06:25 +0900

    feat: WIP | Add "new" command

--- src/main/kotlin/io/github/rokuosan/mint/commands/New.kt
+++ src/main/kotlin/io/github/rokuosan/mint/commands/New.kt
@@ -4,10 +4,14 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.choice
-import com.github.ajalt.mordant.table.table
+import com.github.ajalt.mordant.animation.progressAnimation
+import com.github.ajalt.mordant.terminal.StringPrompt
+import com.github.ajalt.mordant.widgets.Spinner
+import io.github.rokuosan.mint.TERMINAL
 import io.github.rokuosan.mint.fetcher.PaperFetcher
 import io.github.rokuosan.mint.fetcher.PaperFetcherOptions
 import io.github.rokuosan.mint.utils.RandomStringProvider
+import kotlinx.coroutines.*
 
 class New: CliktCommand() {
     private val engine by option("-e", "--engine", help = "Server engines(e.g. Paper, Vanilla)")
@@ -20,34 +24,61 @@ class New: CliktCommand() {
     private val destination by option("-d", "--destination", help = "Output directory")
     private val filename by option("-f", "--filename", help = "Output filename")
 
-    override fun run() {
+    override fun run() = runBlocking {
         when (engine) {
             "paper", "p" -> {
-                echo(table {
-                    body { row("Paper", version) }
-                })
+                val terminal = TERMINAL
                 val bld = if (build != null) {
                     build!!
                 }else {
-                    echo("Checking builds for version $version")
-                    val builds = PaperFetcher().getBuilds(version)?:run{
-                        echo("Invalid version")
-                        return
+                    // Make a progress bar
+                    val progress = terminal.progressAnimation {
+                        text("Fetching builds...")
+                        spinner(Spinner.Dots())
                     }
-                    echo("Found ${builds.size} builds.")
-                    builds.last().toString()
+                    progress.start()
+
+                    // Fetch builds
+                    var bld: List<Int>? = null
+                    val job = launch {
+                        bld = PaperFetcher().getBuilds(version) ?: emptyList()
+                    }
+
+                    // Wait for fetching builds
+                    while (bld == null) {
+                        delay(10)
+                        progress.advance(1)
+                    }
+
+                    // Stop progress bar
+                    job.join()
+                    progress.stop()
+                    terminal.cursor.move {
+                        up(1)
+                        clearLineAfterCursor()
+                    }
+                    terminal.println("Fetching builds... Done")
+
+                    if (bld!!.isEmpty()) {
+                        echo("No builds found.")
+                        return@runBlocking
+                    }
+                    bld!!.last().toString()
                 }
 
                 val dest = if (destination != null) {
                     destination!!
                 }else {
-                    echo("Using random string as destination directory name.")
-                    val randomString = RandomStringProvider.getShortString()
-                    echo("Random string: $randomString")
-                    randomString
+                    RandomStringProvider.getShortString()
                 }
 
-                echo("Downloading Paper $version build $bld to $dest")
+                val pds = StringPrompt(
+                    prompt = "Destination",
+                    terminal = terminal,
+                    default = dest,
+                    showDefault = true,
+                ).ask()?:dest
+                echo("Downloading Paper build $bld[MC:$version] to $pds...")
                 PaperFetcher().download(PaperFetcherOptions(version, bld, dest, filename))
             }
             "vanilla", "v" -> {

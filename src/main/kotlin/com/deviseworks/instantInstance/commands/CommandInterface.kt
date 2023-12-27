54b645f8d2bdf579b49987639d6c4b301fef691b
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-12-27 13:24:57 +0900

    chore: Remove old packages

--- src/main/kotlin/com/deviseworks/instantInstance/commands/CommandInterface.kt
+++
@@ -1,5 +0,0 @@
-package com.deviseworks.instantInstance.commands
-
-interface CommandInterface {
-    fun interact()
-}
--- src/main/kotlin/com/deviseworks/instantInstance/commands/Exit.kt
+++
@@ -1,9 +0,0 @@
-package com.deviseworks.instantInstance.commands
-
-import kotlin.system.exitProcess
-
-class Exit: CommandInterface {
-    override fun interact() {
-        exitProcess(0)
-    }
-}

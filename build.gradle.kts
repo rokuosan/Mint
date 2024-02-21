cf151498a4f41387b56fae23001857cf51db0426
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-02-21 13:52:41 +0900

    build(Gradle): Apply version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,22 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.serialization") version "1.6.21"
-    id("org.jetbrains.dokka") version "1.9.10"
-    id ("com.github.johnrengelman.shadow") version "8.1.1"
-    application
-    java
+    alias(libs.plugins.jvm)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 group = "me.konso"
-java.sourceCompatibility = JavaVersion.VERSION_16
-java.targetCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
-    implementation("com.github.ajalt.clikt:clikt:4.2.1")
+    implementation(libs.bundles.kotlinx)
+    implementation(libs.clikt)
+    implementation(libs.napier)
+    implementation(libs.ktor.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-}
-
-application {
-    mainClass.set("io.github.rokuosan.mint.MainKt")
-}
-
-tasks.withType<KotlinCompile>{
-    kotlinOptions{
-        jvmTarget = "11"
-    }
-}
-
-tasks.dokkaHtml.configure{
-    outputDirectory.set(layout.buildDirectory.dir("docs"))
-    moduleName.set("Mint")
-}
-
-tasks.getByName<JavaExec>("run"){
-    standardInput = System.`in`
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "11"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
+    testImplementation(libs.bundles.test)
 }
